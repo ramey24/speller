296e21c54a6ccdf8e06951821d4f814ee6031531
ramey24 <51526742+ramey24@users.noreply.github.com>
2019-06-13 14:31:32 +0630

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "dictionary.h"
 
@@ -22,10 +23,23 @@ node;
 // Represents a trie
 node *root;
 
+// num of words in dictionary
+int numWords = 0;
+
+// node of the alphabet being computed
+node *current;
+    
+// previous node for experimental purposes
+node *previous;
+    
+char *alphabet = "abcdefghijklmnopqrstuvwxyz'";
 
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
+    // num of words immediately after opening a new dicionary is 0; 
+    numWords = 0;
+    
     // Initialize trie
     root = malloc(sizeof(node));
     if (root == NULL)
@@ -49,14 +63,6 @@ bool load(const char *dictionary)
     // Buffer for a word
     char word[LENGTH + 1];
     
-    // node of the alphabet being computed
-    node *current;
-    
-    // previous node for experimental purposes
-    node *previous;
-    
-    char *alphabet = "abcdefghijklmnopqrstuvwxyz'";
-
     // Insert words into trie
     // fgets() = get the word in each line
     while (fgets(word, LENGTH + 1, file) != NULL)
@@ -96,7 +102,8 @@ bool load(const char *dictionary)
                 current = current->children[index];
             }
         }
-        root->is_word = true;
+        current->is_word = true;
+        numWords += 1;
     }
 
     // Close dictionary
@@ -109,15 +116,54 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
+    if (numWords != 0)
+    {
+        return numWords;
+    }
     return 0;
 }
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
-    return false;
+    for (int i = 0; i < strlen(word); i++)
+    {
+        // change to lower case
+        char currletter = tolower(word[i]);
+        
+        // find the corresponding index of the letter in the string alphabet
+        char *letter = strchr(alphabet, currletter);
+        int index = (int)(letter - alphabet);
+        
+        if (i == 0)
+        {
+            // if the first letter is not in the alphabet or isn't a semicolon
+            if (!root->children[index])
+            {
+                return false;
+            }
+            else
+            {
+                current = root->children[index];
+            }
+        }
+        else
+        {
+            if (!current->children[index])
+            {
+                return false;
+            }
+            else
+            {
+                current = current->children[index];
+            }
+        }
+    }
+    if (current->is_word == false)
+    {
+        return false;
+    }
+    return true;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
